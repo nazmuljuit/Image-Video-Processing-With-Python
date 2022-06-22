dc7b1a370b7f1097f3c295cf12949876abf91808
Nazmul Hossain <nazmuljuit@gmail.com>
2022-06-22 14:17:14 +0600

    update

--- faceDetector.py
+++ faceDetector.py
@@ -1,7 +1,9 @@
 import cv2
-face_casecade = cv2.CasecadeClassifier("haarcascade_frontalface_default.xml")
+face_casecade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img = cv2.imread("photo.jpg")
 gray_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+face = face_casecade.detectMultiScale(gray_image)
 cv2.imshow('Gray',gray_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
