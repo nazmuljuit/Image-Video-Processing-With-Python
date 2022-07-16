35f9c9d47c686623e24a24eb4c43c0528f1a78fb
Nazmul Hossain <nazmuljuit@gmail.com>
2022-07-16 19:30:24 +0600

    update

--- faceDetector.py
+++ faceDetector.py
@@ -4,9 +4,12 @@ img = cv2.imread("photo.jpg")
 gray_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 face = face_casecade.detectMultiScale(gray_image,scaleFactor=1.05,minNeighbors=5)
+
+for x,y,w,h in face:
+  img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
 print(type(face))
 print(face)
-cv2.imshow('Gray',gray_image)
+cv2.imshow('Gray',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
